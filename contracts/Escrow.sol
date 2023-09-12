ae8831fccf83c42efb16863618dc9960a9e0200c
ChristianRukundo <siboruremarukundoc1@gmail.com>
2023-09-12 11:37:21 +0200

    basic functionality

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -2,13 +2,25 @@
 pragma solidity ^0.8.0;
 
 interface IERC721 {
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _id
-    ) external;
+    function transferFrom(address _from, address _to, uint256 _id) external;
 }
 
 contract Escrow {
+    address nftAddress;
+    address payable public seller;
+    address public inspector;
+    address public lender;
 
+    constructor(
+        address _nftAddress,
+        address payable _seller,
+        address _inspector,
+        address _lender
+    ) public {
+        nftAddress = _nftAddress;
+        seller = _seller;
+        inspector = _inspector;
+        lender = _lender;
+    }
+    
 }
